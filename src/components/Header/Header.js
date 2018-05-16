3a4c89279ff49fb35a80c834462a6887a98f1efe
Piotr Ptak <robakzdobywca@gmail.com>
2018-05-16 22:02:12 +0200

    header le piaf

---
+++ src/components/Header/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+  return (
+    <div className="Header">
+      <h1><span className="balloon-red">Le </span>piaf</h1>
+    </div>
+  );
+};
+
+export default Header;
